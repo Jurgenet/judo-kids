10724db186f9ad7ad388ca39cf77eef294f74e82
y.ivanchenko <jurgenet@gmail.com>
2023-01-16 16:48:20 +0300

    update SEO links

--- components/AppFooter/index.vue
+++ components/AppFooter/index.vue
@@ -6,13 +6,13 @@
       <div class="text-2xl font-bold">подробно по телефону:</div>
       <div class="phoneNumber text-6xl">+7 920 267 66 12</div>
       <div class="flex align-items-center">
-        <a href="https://ok.ru/profile/561837567437" target="_blank">
+        <a href="https://ok.ru/profile/561837567437" target="_blank" rel="nofollow">
           <img class="okPicture" alt="ok icon" src="~/assets/images/ok.png" />
         </a>
-        <a href="https://vk.com/id41078417" target="_blank">
+        <a href="https://vk.com/id41078417" target="_blank" rel="nofollow">
           <img class="vkPicture" alt="vk icon" src="~/assets/images/vk.png" />
         </a>
-        <a href="https://www.youtube.com/channel/UClynBflVWcOSgt_auYnNrJQ" target="_blank">
+        <a href="https://www.youtube.com/channel/UClynBflVWcOSgt_auYnNrJQ" target="_blank" rel="nofollow">
           <img class="ytPicture" alt="youtube icon" src="~/assets/images/youtube.png" />
         </a>
       </div>
